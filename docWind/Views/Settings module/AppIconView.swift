3a10fb1937b34a7d4107ead607f9716e70878fe6
s4rv4d <sarvadshetty@gmail.com>
2020-07-27 15:41:42 +0530

    fixed apple issue, and app icon tap issue

--- docWind/Views/Settings module/AppIconView.swift
+++ docWind/Views/Settings module/AppIconView.swift
@@ -11,8 +11,9 @@ import SwiftUI
 struct AppIconView: View {
     
     // MARK: - Properties
-    let appIcons = ["blackBlue", "blackGreen", "blackRed", "blackYellow"]
-    let appIconName = ["Black and Blue", "Black and Green", "Black and Red", "Black and Yellow"]
+    @State var appIcons = ["blackBlue", "blackGreen", "blackRed", "blackYellow"]
+    @State var appIconName = ["Black and Blue", "Black and Green", "Black and Red", "Black and Yellow"]
+    @State var selected = UIApplication.shared.alternateIconName
     
     var body: some View {
         VStack {
@@ -21,28 +22,45 @@ struct AppIconView: View {
             List {
                 Section(footer: Text("More to come soon :)")) {
                     ForEach(appIcons.indices, id: \.self) { i in
-                        HStack{
-                            Image(self.appIcons[i])
-                            .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            .frame(width: 50, height: 50)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                            .shadow(color: .secondary, radius: 2, x: 0, y: 0)
-                            
-                            Text(self.appIconName[i])
-                                .foregroundColor(.primary)
-                            Spacer()
-                        }.onTapGesture {
+                        Button(action: {
+                            self.selected = self.appIcons[i]
                             FeedbackManager.mediumFeedback()
                             self.changeIcon(to: self.appIcons[i])
-                        }
+                        }){
+                            HStack {
+                                Image(self.appIcons[i])
+                                .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                                .shadow(color: .secondary, radius: 2, x: 0, y: 0)
+                                
+                                Text(self.appIconName[i])
+                                    .foregroundColor(.primary)
+                                Spacer()
+                                
+                                if self.selected == self.appIcons[i] {
+                                    Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
+                                }
+                            }.contentShape(Rectangle())
+                            
+                        }.buttonStyle(PlainButtonStyle())
                     }
-                    Button("Reset to default") {
+                    Button(action: {
+                        self.selected = nil
                         UIApplication.shared.setAlternateIconName(nil) { (error) in
                             if let error = error {
                                 print("App icon failed to change due to \(error.localizedDescription)")
                               } else {
                                 print("App icon changed successfully")
+                            }
+                        }
+                    }) {
+                        HStack{
+                            Text("Reset to default")
+                            Spacer()
+                            if self.selected == nil {
+                                Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
                             }
                         }
                     }
