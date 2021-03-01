e17e3702783ad1ac8cebf942957e5883c2d2887d
s4rv4d <sarvadshetty@gmail.com>
2021-03-01 11:48:55 +0300

    updated dependency page

--- docWind/Views/Settings module/DependecyPageView.swift
+++ docWind/Views/Settings module/DependecyPageView.swift
@@ -53,6 +53,62 @@ struct DependecyPageView: View {
                     }
                     // --> 1
                     
+                    // --> 2
+                    VStack(alignment: .leading, spacing: 5) {
+                        HStack {
+                            Text("MetalPetal")
+                                .font(.largeTitle)
+                                .fontWeight(.semibold)
+                            Spacer()
+                            SFSymbol.exclamationMarkCircle
+                                .foregroundColor(Color(tintColor))
+                                .onTapGesture {
+                                    SettingsHelper.openUrl(url: "https://github.com/MetalPetal/MetalPetal.git")
+                                }
+
+                        }
+                        .padding([.bottom])
+                        Text("by MetalPetal")
+                        Text("https://github.com/MetalPetal/MetalPetal.git")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text("MIT License")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                            .padding(.bottom)
+                        
+                    }
+                    .settingsBackground()
+                    // --> 2
+                    
+                    // --> 3
+                    VStack(alignment: .leading, spacing: 5) {
+                        HStack {
+                            Text("Throttler")
+                                .font(.largeTitle)
+                                .fontWeight(.semibold)
+                            Spacer()
+                            SFSymbol.exclamationMarkCircle
+                                .foregroundColor(Color(tintColor))
+                                .onTapGesture {
+                                    SettingsHelper.openUrl(url: "https://github.com/boraseoksoon/Throttler.git")
+                                }
+
+                        }
+                        .padding([.bottom])
+                        Text("by Jang Seoksoon")
+                        Text("https://github.com/boraseoksoon/Throttler.git")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text("MIT License")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                            .padding(.bottom)
+                        
+                    }
+                    .settingsBackground()
+                    // --> 3
+                    
                     Spacer()
                 }
                 .padding(.top)
